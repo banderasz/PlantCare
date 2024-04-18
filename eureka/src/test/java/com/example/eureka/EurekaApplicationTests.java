f8dcbe60c2eca29273cf8f94294ca61287e0ef4b
András Heszler <andras.heszler@tngtech.com>
2024-04-18 15:01:27 +0200

    Add Eureka service

--- eureka/src/test/java/com/example/eureka/EurekaApplicationTests.java
+++ eureka/src/test/java/com/example/eureka/EurekaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.plantcare;
+package com.example.eureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PlantCareApplicationTests {
+class EurekaApplicationTests {
 
     @Test
     void contextLoads() {
